b35b11bbcba714cfc0159dfbfd66151dcf7e5463
Sumanta-Jena <jena.sumanta652@gmail.com>
2022-07-26 15:19:05 +0530

    created paths

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+if(process.env.NODE_ENV !== "production"){
+    require('dotenv').config();
+}
+
+console.log(process.env.SECRET)
+
 const express=require('express');
 const app=express();
 const path=require('path');
+app.use(express.static('public'));
+//var  express=require('partials');
 // const mongoose=require('mongoose');
 // mongoose.connect('mongodb://localhost:27017/products')
 //   .then(()=>
@@ -26,6 +34,25 @@ app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
 app.get('/',(req,res)=>
-res.send("Home Page"))
+res.render("home.ejs"))
+
+app.get('/aboutUs',(req,res)=>
+res.render("home.ejs"))
+
+app.get('/register',(req,res)=>
+res.render("register.ejs"))
+
+app.get('/logIn',(req,res)=>
+res.render("login.ejs"))
+
+app.get('/adopt',(req,res)=>
+res.render("adopt.ejs"))
+
+app.get('/adopt/new',(req,res)=>
+//res.render("adopt.ejs"))
+res.send("adopt_new"))
+
+app.get('/donate',(req,res)=>
+res.render("donate.ejs"))
 
 app.listen(3000,()=>console.log("Server Up and Running"));
